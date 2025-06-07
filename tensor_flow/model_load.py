a68a6f0f7ea21fa394dc1636308d1715f4a84b1e
Oleg Potapenko <oleg@example.com>
2025-06-07 03:05:24 -0400

    Small changes

---
+++ tensor_flow/model_load.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import logging
+import numpy as np
+
+if __name__ == "__main__":
+    logger = tf.get_logger()
+    logger.setLevel(logging.ERROR)
+    
+    CLASS_NAMES = {
+        0: 'int',
+        1: 'float',
+        2: 'boolean',
+        3: 'date',
+        4: 'datetime',
+        5: 'uuid',
+        6: 'string'
+    }
+    VOCAB_SIZE: int = 128
+    MAX_LENGTH: int = 100
+    
+    def preprocess_string(input: str) -> np.ndarray:
+        encoded = [ord(c) % VOCAB_SIZE for c in input[:MAX_LENGTH].upper()]
+        if len(encoded) < MAX_LENGTH:
+            encoded.extend([0] * (MAX_LENGTH - len(encoded)))
+        
+        return np.array(encoded)
+
+    saved_keras_model_filepath = '/home/oleg/Developer/GitHub/TensorFlow/TensorFlow/tensor_flow/best_model_20250606-220704.keras'
+    model = tf.keras.models.load_model(saved_keras_model_filepath)
+    
+    
+    # test_strings = [
+    #     "42",
+    #     "3.14159",
+    #     "true",
+    #     "2024-01-15",
+    #     "2024-01-15 14:30:00",
+    #     "user@example.com",
+    #     "a20d5384-3110-4567-b5a9-9b581a40e1f8",
+    #     "+1-555-123-4567",
+    #     '{"name": "John"}',
+    #     "Hello, World!"
+    # ]
+
+    x = preprocess_string("2024-01-15 14:30:00")
+    x = np.expand_dims(x, axis=0)
+    print(x.shape)
+    predictions = model.predict(x)[0]
+    print(predictions.shape)
+    max_index = int(np.argmax(predictions)) 
+    print("Predicted class: ", CLASS_NAMES[max_index])
+    print("Probability: ", predictions[max_index])
+    # results = {}
+    # for i, prob in enumerate(predictions):
+    #     results[CLASS_NAMES[i]] = float(prob)
+    # print(results)
